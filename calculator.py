f57b3d65436c30a0106db4c7f22cfd480d77e9be
khanTaufeeq <taufeeqyouth@gmail.com>
2025-04-18 21:30:16 +0530

    refracted code

--- calculator.py
+++ calculator.py
@@ -3,14 +3,15 @@ import re
 def add_strings(s):
     if s == '':
         return 0
-    delimiter = s[2]
-
-    sub_string = s.split('\n')
-
-    sub_num_string = sub_string[1]
-
-    delimiter = re.escape(delimiter)
-    splitted_string = re.split(delimiter,sub_num_string)
+    if s[0] == '/' and s[1] == '/':
+        delimiter = s[2]
+        sub_string = s.split('\n')
+        sub_num_string = sub_string[1]
+        delimiter = re.escape(delimiter)
+        splitted_string = re.split(delimiter,sub_num_string)
+    
+    else:
+        splitted_string = re.split(r'[,\n]',s)
 
     result = add_multiple_number_string(splitted_string)
 
