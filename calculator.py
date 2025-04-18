a623c1bc51b439d1b71f4a8e5f46905fe5f64901
khanTaufeeq <taufeeqyouth@gmail.com>
2025-04-18 20:54:31 +0530

    refracted code

--- calculator.py
+++ calculator.py
@@ -3,10 +3,15 @@ import re
 def add_strings(s):
     if s == '':
         return 0
-    sum_num = 0
     splitted_string = re.split(r'[\n,]',s)
 
-    for i in splitted_string:
-        sum_num += int(i)
+    result = add_multiple_number_string(splitted_string)
 
+    return result
+
+
+def add_multiple_number_string(s):
+    sum_num = 0
+    for i in s:
+        sum_num += int(i)
     return sum_num
