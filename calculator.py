7f6d7a8c77386b531d54a0c08d528e9cb26561c9
khanTaufeeq <taufeeqyouth@gmail.com>
2025-04-18 13:56:51 +0530

    updated new line delimiter addition

--- calculator.py
+++ calculator.py
@@ -5,6 +5,7 @@ def add_strings(s):
     splitted_string = re.split(r'[,\n]', s)
     sum_num = 0
     for i in splitted_string:
-        sum_num += int(i)
+        if i != '':
+            sum_num += int(i)
 
     return sum_num
